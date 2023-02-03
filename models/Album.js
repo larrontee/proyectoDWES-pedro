7346f63bc543c71cd9fbc7b926258f977cdf0c11
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2023-02-03 10:27:31 +0100

    Update and rename album.js to Album.js

--- models/Album.js
+++ models/Album.js
@@ -1,20 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var User = require('../models/user.js');
+var Cancion = require('../models/Cancion.js');
+var User = require('../models/User.js');
 var AlbumSchema = new Schema({
     nombre:{
         type:String,
         required: true
     },
-    autor:{
+    autor:[{
         type: Schema.ObjectId,
         ref: 'User',
         required: true
-    },
+    }],
     numeroCanciones:{
         type:Number,
         required: true
     },
+    canciones:[{
+        type:Schema.ObjectId,
+        ref: 'Cancion',
+        required: true
+    }],
     descripcion:{
         type:String,
         required:false
