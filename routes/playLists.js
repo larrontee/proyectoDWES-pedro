e3ba79b7bcd5917d5ff7e0e3f88446d5457f7bc4
Manubetis <alum.mjaner.2021@iesaixar.org>
2023-02-13 13:32:32 +0100

    Actualización routes playlist

--- routes/playLists.js
+++ routes/playLists.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET de una única playlist por su Id 
+router.get('/:id', function (req, res, next) {
+  playlist.findById(req.params.id, function (err, playlist) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(playlist);
+  });
+});
+
+// POST de una nueva playlist 
+router.post('/', function (req, res, next) {
+  playlist.create(req.body, function (err, playlist) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+// PUT de una playlist existente identificado por su Id 
+router.put('/:id', function (req, res, next) {
+  playlist.findByIdAndUpdate(req.params.id, req.body, function (err, playlist) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+// DELETE de una playlist existente identificado por su Id 
+router.delete('/:id', function (req, res, next) {
+  playlist.findByIdAndDelete(req.params.id, function (err, playlist) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+// GET del listado de posts ordenados por fecha de publicación 
+router.get('/', function (req, res, next) {
+  Post.find().sort('-publicationdate').populate('nombre').populate('canciones', {
+    _id: 0,
+    nombre: 1
+  }).exec(function (err, playlist) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(playlist);
+  });
 });
 
 module.exports = router;
