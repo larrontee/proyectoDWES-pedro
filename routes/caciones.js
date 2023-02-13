5cf98726061ada1618494dfed000d64e4138e319
Larronte <alum.plarrubiam.2020@iesalixar.org>
2023-02-13 12:33:39 +0100

    modelo cancion modificado

--- routes/caciones.js
+++ routes/caciones.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
+
+router.get('/', function (req, res, next) {
+  User.find().sort('').exec(function (err, users) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(users);
+  });
+});
