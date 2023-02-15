32abb9341cc8daf84404ae3bb79ab3ae330000da
Javier-Coronel <alum.jcoronelo.2021@iesalixar.org>
2023-02-15 01:36:13 +0100

    Update albunes.js

--- routes/albunes.js
+++ routes/albunes.js
@@ -16,21 +16,32 @@ router.get('/searchbyseguidores', function(req, res, next) {
     else res.status(200).json(albums);
   });
 });
-router.get('/:nombre', function(req, res, next) {
-  console.log("asd");
-  Album.findOne({nombre: req.params.nombre}, function(err, albums) {
+router.get('/searchByNombre/:nombre', function(req, res, next) {
+  Album.find({nombre: req.params.nombre}, function(err, albums) {
+    if(err) res.status(500).send(err);
+    else res.status(200).json(albums);
+  });
+});
+router.get('/:id', function(req, res, next) {
+  Album.findById(req.params.id, function(err, albums) {
     if(err) res.status(500).send(err);
     else res.status(200).json(albums);
   });
 });
 router.post('/', function(req, res, next) {
-  Album.create(req.body, function(err, userinfo) {
+  Album.create(req.body, function(err, albums) {
+    if(err)res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+router.put('/:id', function(req, res, next) {
+  Album.findByIdAndUpdate(req.params.id, req.body, function(err,albums){
     if(err)res.status(500).send(err);
     else res.sendStatus(200);
   });
 });
 router.delete('/:id', function(req, res, next) {
-  Album.findByIdAndDelete(req.params.id, function (err, userinfo) {
+  Album.findByIdAndDelete(req.params.id, function (err, albums) {
     if(err)res.status(500).send(err);
     else res.sendStatus(200);
   });
