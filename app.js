b49436caf10520810586bf439a5ecc5460002d17
Manuel Fernández Serrano <alum.mfernandezs.2021@iesalixar.org>
2023-02-28 12:03:17 +0100

    Añadimos express-validator y validamos los users

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const {body, validationResult} = require('express-validator');
+const app = express();
 require('dotenv').config();
 
-// let indexRouter = require('./routes/index');
-// let usersRouter = require('./routes/users');
 let bodyparser = require('body-parser');
 let userModel = require('./models/User');
 let songtModel = require('./models/Cancion');
@@ -18,7 +17,7 @@ let albumsRouter = require('./routes/albunes');
 let songsRouter = require('./routes/canciones');
 let playlistRouter = require('./routes/playLists');
 
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); //requerido para quitar el warning
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
   .then(() => console.log('connection successful'))
@@ -26,39 +25,38 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
 
 mongoose.connection;
 
-let app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/albunes', albumsRouter);
 app.use('/songs', songsRouter);
 app.use('/playlist', playlistRouter);
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,52 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 var db = mongoose.connection;
 
-// POST de un nuevo usuario
-router.post('/', function(req, res, next) {
-  User.create(req.body, function(err, user) {
-    if (err) res.status(500).send(err);
-    else res.sendStatus(200);
-    });
-});
+//POST de nuevo usuario con validaciones
+router.post('/',
+  body('usuario')
+    .exists()
+    .isString(),
+  body('password', "La contraseña debe ser alfanumerica y tener más de 8 caracteres")
+    .exists()
+    .isAlphanumeric()
+    .isLength( {min:8} ),
+  body('email', "Este campo debe ser un email")
+    .exists()
+    .isEmail(),
+  body('fechaRegistro')
+    .optional()
+    .isDate(),
+  body('tipo')
+    .optional(),
+  body('numSeguidores', "Este campo debe ser numérico")
+    .optional()
+    .isNumeric(),
+  body('seguidos', "Este campo debe ser un array")
+    .optional()
+    .isArray(),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array()})
+    }
+
+    User.create({
+      
+      usuario: req.body.usuario,
+      password: req.body.password,
+      email: req.body.email,
+      fechaRegistro: req.body.fechaRegistro,
+      tipo: req.body.tipo,
+      numSeguidores: req.body.numSeguidores,
+      seguidos: req.body.seguidos
+
+    }).then(user => res.json(user));
+  }
+);
 
 // GET de un único usuario por su nombre de usuario
 router.get('/:usuario', function(req, res, next) {
@@ -20,7 +57,7 @@ router.get('/:usuario', function(req, res, next) {
   });
 });
 
-// Listado de los seguidos de un usuario
+// GET de un listado de los seguidos de un usuario
 router.get('/:usuario/listado_seguidos', function(req, res, next) {
   User.find().populate("seguidos", {
     _id:0,
